7524e44b09089c536f9319f6fdf7259a868482da
louissankey <lmsankey@ucdavis.edu>
2021-03-12 16:31:10 -0800

    migrated to Androidx and updated compatibility

--- app/src/main/java/com/example/louissankey/darkhorselayouts/Activity/DraftPlayer.java
+++ app/src/main/java/com/example/louissankey/darkhorselayouts/Activity/DraftPlayer.java
@@ -4,9 +4,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
